ee088b1eaf422f212d3e546a8ac2bcc7c0b97a04
JH8459 <kk_ong2233@naver.com>
2024-01-15 23:13:17 +0900

    Setting: [#1] Github Setting

---
+++ src/test/java/jh8459/Lottery/LotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package jh8459.Lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
